6a6c4d20ab8a859d57bbc17880352740eaf592e9
Raul Gonzalez <nawn1994@gmail.com>
2018-05-15 00:03:12 -0700

    Create new form to be overlay

---
+++ UniversalCrosshair/CrosshairOverlay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UniversalCrosshair
+{
+    public partial class CrosshairOverlay : Form
+    {
+        public CrosshairOverlay()
+        {
+            InitializeComponent();
+        }
+
+        private void CrosshairOverlay_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Wheat;
+            this.TransparencyKey = Color.Wheat;
+        }
+    }
+}
