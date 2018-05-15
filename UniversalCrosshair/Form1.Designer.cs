ed3e2e91c7e4d35eb4be793a085a1f3b2218fe2f
Raul Gonzalez <nawn1994@gmail.com>
2018-05-15 00:03:43 -0700

    Begin Adding Functionality

--- UniversalCrosshair/Form1.Designer.cs
+++ UniversalCrosshair/Form1.Designer.cs
@@ -32,8 +32,8 @@
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.processSelector = new System.Windows.Forms.ComboBox();
+            this.refreshProcessBtn = new System.Windows.Forms.Button();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
@@ -67,7 +67,7 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.refreshProcessBtn, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
@@ -81,7 +81,7 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.label1);
-            this.flowLayoutPanel1.Controls.Add(this.comboBox1);
+            this.flowLayoutPanel1.Controls.Add(this.processSelector);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
@@ -100,24 +100,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Process Name";
             // 
-            // comboBox1
+            // processSelector
             // 
-            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(3, 22);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(228, 21);
-            this.comboBox1.TabIndex = 1;
+            this.processSelector.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.processSelector.FormattingEnabled = true;
+            this.processSelector.Location = new System.Drawing.Point(3, 22);
+            this.processSelector.Name = "processSelector";
+            this.processSelector.Size = new System.Drawing.Size(228, 21);
+            this.processSelector.TabIndex = 1;
             // 
-            // button1
+            // refreshProcessBtn
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(240, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(73, 44);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Refresh";
-            this.button1.UseVisualStyleBackColor = true;
+            this.refreshProcessBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.refreshProcessBtn.Location = new System.Drawing.Point(240, 3);
+            this.refreshProcessBtn.Name = "refreshProcessBtn";
+            this.refreshProcessBtn.Size = new System.Drawing.Size(73, 44);
+            this.refreshProcessBtn.TabIndex = 1;
+            this.refreshProcessBtn.Text = "Refresh";
+            this.refreshProcessBtn.UseVisualStyleBackColor = true;
+            this.refreshProcessBtn.Click += new System.EventHandler(this.refreshProcessBtn_Click);
             // 
             // tableLayoutPanel3
             // 
@@ -161,6 +162,7 @@
             this.button2.TabIndex = 0;
             this.button2.Text = "Enable";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -171,6 +173,7 @@
             this.button3.TabIndex = 1;
             this.button3.Text = "Disable";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -195,8 +198,8 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox processSelector;
+        private System.Windows.Forms.Button refreshProcessBtn;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
         private System.Windows.Forms.Button button2;
--- UniversalCrosshair/Form1.cs
+++ UniversalCrosshair/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace UniversalCrosshair
 {
     public partial class Form1 : Form
     {
+
+        CrosshairOverlay overlayForm = new CrosshairOverlay();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void refreshProcessBtn_Click(object sender, EventArgs e)
+        {
+            string[] undesired = { "svchost", "conhost", "spoolsv", "lsass" };
+            processSelector.Items.Clear();
+            Process[] localAll = Process.GetProcesses();
+
+            foreach (var item in localAll)
+            {
+                bool shouldAdd = true;
+                string processName = item.ProcessName;
+                foreach (string process in undesired)
+                {
+                    if (process == processName)
+                    {
+                        shouldAdd = false;
+                    }
+                }
+                
+                if (shouldAdd)
+                {
+                    processSelector.Items.Add(processName);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            overlayForm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            overlayForm.Hide();
         }
     }
 }
